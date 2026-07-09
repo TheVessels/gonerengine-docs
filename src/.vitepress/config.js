1fcfa0c29faa36dd909c36aff9f141ad164f501e
KrisGra <39800337+krisgrant@users.noreply.github.com>
2026-07-09 15:49:40 +0300

    Add social links to nav bar and various theme changes

--- src/.vitepress/config.js
+++ src/.vitepress/config.js
@@ -2,29 +2,38 @@ import { defineConfig } from "vitepress";
 import { withSidebar } from "vitepress-sidebar";
 
 const config = defineConfig({
-    title: "GonerEngine Docs",
-    description: "An in-development modders guide",
-    base: "/gonerengine-docs/",
-    vite: {
-        configFile: "vite.config.js"
+  title: "GonerEngine Docs",
+  description: "An in-development modders guide",
+  base: "/gonerengine-docs/",
+  vite: {
+    configFile: "vite.config.js",
+  },
+  markdown: {
+    languageAlias: {
+      gml: "python",
     },
-    markdown: {
-        languageAlias: {
-            gml: "python"
-        },
+  },
+  themeConfig: {
+    logo: "/img/logo.png",
+    socialLinks: [
+      // You can add any icon from simple-icons (https://simpleicons.org/):
+      { icon: "github", link: "https://github.com/TheVessels/GonerEngine" },
+      { icon: "discord", link: "https://discord.gg/2Ea2bpQGq5" },
+    ],
+    editLink: {
+      pattern: "https://github.com/TheVessels/gonerengine-docs/edit/master/src/:path",
+      text: "Edit this page on GitHub",
     },
-    themeConfig: {
-        logo: "/img/logo.png"
-    }
+  },
 });
 
 const sidebarConfig = {
-    documentRootPath: "/src",
-    collapsed: true,
-    collapseDepth: 3,
-    useTitleFromFileHeading: true,
-    includeRootIndexFile: false,
-    includeFolderIndexFile: true,
+  documentRootPath: "/src",
+  collapsed: true,
+  collapseDepth: 3,
+  useTitleFromFileHeading: true,
+  includeRootIndexFile: false,
+  includeFolderIndexFile: true,
 };
 
 export default withSidebar(config, sidebarConfig);
